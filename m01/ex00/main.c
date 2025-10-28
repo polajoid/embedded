a7facba1c82ccba77c3d5ae797cac41fd0975347
Jean-Baptiste Lepany <jlepany@f1br8s14.clusters.42paris.fr>
2025-10-28 12:39:20 +0100

    use of volatile necessary when optimization flag used inside makefile. might need to change to 32 bits int

--- m01/ex00/main.c
+++ m01/ex00/main.c
@@ -1,13 +1,16 @@
 #include "module_one.h"
 
-void	my_ms_delay(unsigned int ms)
+void	my_delay_ms(unsigned int ms)
+//creating my own delay
 {
-	unsigned int x;
-	while (ms--)
+	volatile unsigned int x;
+	//use of volatile is necessary because flag optimization will prune the x
+	while (ms)
 	{
-		x = 2600;
-		while (x--)
-			;
+		x = 1100;
+		while (x)
+			x--;
+		ms--;
 	}
 }
 
@@ -19,7 +22,7 @@ int main(void)
 	
 	while (1)
 	{
-		my_ms_delay(500);
+		my_delay_ms(500);
 		//Frequency is the number of occurrences of a repeating event per unit of time
 		//If we want 1hz we want an event to repeat each second so each 1000ms
 		//Hence every 500ms, the LED switch off or switch on so that every second the LED switch on
