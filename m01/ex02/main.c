552ffd6c511fcb6ac3e39780fce0366352eb9a58
Jean-Baptiste Lepany <jlepany@f1br9s7.clusters.42paris.fr>
2025-10-29 12:39:12 +0100

    add comment about compiler

--- m01/ex02/main.c
+++ m01/ex02/main.c
@@ -15,8 +15,8 @@ int main(void)
 	TCCR1B |= (1 << WGM13 | 1 << WGM12); 
 	TCCR1A |= (1 << WGM11);
 
-	//we want a cycle to be 1hz so : F_CPU / (1mhz * pres) - 1 
-	ICR1 = 62499;
+	//we want a cycle to be 1hz so : F_CPU / (1mhz * pres) - 1 -> 62499
+	ICR1 = 62499; //compiler set up first high then low 8bit registry so no problem here
 	//all we need now is to set up a TOP value (OCR1A) for toggling the port : 10 % of 62499
 	OCR1A = 6250;
 	while (1) 
