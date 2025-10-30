a74bc218843269a4743f79082c571503e4b51001
Jean-Baptiste Lepany <jlepany@f1br9s7.clusters.42paris.fr>
2025-10-30 11:19:08 +0100

    rather simple for now

--- m03/ex00/main.c
+++ m03/ex00/main.c
@@ -1,38 +1,18 @@
-#include "module_two.h"
+#include "module_three.h"
 
-void	uart_tx(char c)
-{
-	if (c >= 'a' && c <= 'z')
-		c -= 32;
-	else if (c >= 'A' && c <= 'Z')
-		c += 32;
-	while (!(UCSR0A & (1 << UDRE0)))
-		;							
-	UDR0 = c;
-}
-
-void	uart_init(unsigned int ubrr)
-{
-	UCSR0A |= (1 << U2X0); //double speed asynchronous
-	UCSR0B |= (1 << TXEN0 | 1 << RXEN0 | 1 << RXCIE0); //RXCIE0 enable interrupt when RX buff is complete
-	
-	UCSR0C |= (1 << UCSZ01 | 1 << UCSZ00);
-	UCSR0B &= ~(1 << UCSZ02); //both line to make 8bits tx and rx
-
-	UBRR0H = (unsigned char)(ubrr >> 8); 	
-	UBRR0L = (unsigned char)ubrr;
-	sei();
-}
+/* Purpose write a program controlling LED RGD D5 */
 
 int main(void)
 {
-	uart_init(16);
+	SET_OUT(D, 5);
 	while (1)
 	{
+		PORTD = (1 << PD5);
+		_delay_ms(1000);
+		PORTD = (1 << PD6);
+		_delay_ms(1000);
+		PORTD = (1 << PD3);
+		_delay_ms(1000);
 	}
 }
 
-ISR(USART_RX_vect)
-{
-	uart_tx(UDR0);
-}
