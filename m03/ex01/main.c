7274b927c6985b39bda0d063956d2094ad77f356
Jean-Baptiste Lepany <jlepany@f1br9s7.clusters.42paris.fr>
2025-10-30 11:40:06 +0100

    easy for now

--- m03/ex01/main.c
+++ m03/ex01/main.c
@@ -5,6 +5,9 @@
 int main(void)
 {
 	SET_OUT(D, 5);
+	SET_OUT(D, 6);
+	SET_OUT(D, 3);
+	/* input mode because output mode burns my eyes */
 	while (1)
 	{
 		PORTD = (1 << PD5);
@@ -12,6 +15,14 @@ int main(void)
 		PORTD = (1 << PD6);
 		_delay_ms(1000);
 		PORTD = (1 << PD3);
+		_delay_ms(1000);
+		PORTD = (1 << PD5 | 1 << PD6); //new colors are just a combinais of RGB
+		_delay_ms(1000);
+		PORTD = (1 << PD6 | 1 << PD3);
+		_delay_ms(1000);
+		PORTD = (1 << PD5 | 1 << PD3);
+		_delay_ms(1000);
+		PORTD = (1 << PD5 | 1 << PD6 | 1 << PD3);
 		_delay_ms(1000);
 	}
 }
